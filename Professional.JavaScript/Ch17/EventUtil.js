9044e5b3215b26dd9e826b02339d058025bde1cd
cubika <receiversume@163.com>
2014-06-07 15:58:37 +0800

    summarize for chapter 17 of Professional.Javascript

--- Professional.JavaScript/Ch17/EventUtil.js
+++ Professional.JavaScript/Ch17/EventUtil.js
@@ -1,16 +1,16 @@
 var EventUtil = {
 
     addHandler: function(element, type, handler){
-        if (element.addEventListener){
+        if (element.addEventListener){ //现代浏览器
             element.addEventListener(type, handler, false);
-        } else if (element.attachEvent){
+        } else if (element.attachEvent){ //IE
             element.attachEvent("on" + type, handler);
         } else {
             element["on" + type] = handler;
         }
     },
     
-    getButton: function(event){
+    getButton: function(event){	//触发当前事件的鼠标按键
         if (document.implementation.hasFeature("MouseEvents", "2.0")){
             return event.button;
         } else {
@@ -20,16 +20,16 @@ var EventUtil = {
                 case 3:
                 case 5:
                 case 7:
-                    return 0;
+                    return 0; //鼠标左键
                 case 2:
                 case 6:
-                    return 2;
-                case 4: return 1;
+                    return 2; //鼠标右键
+                case 4: return 1; //鼠标中键
             }
         }
     },
     
-    getCharCode: function(event){
+    getCharCode: function(event){ //获取按下的按键码，charCode,keyCode,which
         if (typeof event.charCode == "number"){
             return event.charCode;
         } else {
@@ -37,7 +37,7 @@ var EventUtil = {
         }
     },
     
-    getClipboardText: function(event){
+    getClipboardText: function(event){	//剪贴板
         var clipboardData =  (event.clipboardData || window.clipboardData);
         return clipboardData.getData("text");
     },
@@ -46,7 +46,7 @@ var EventUtil = {
         return event ? event : window.event;
     },
     
-    getRelatedTarget: function(event){
+    getRelatedTarget: function(event){ //相关节点，如mouseover事件中鼠标指针离开的节点
         if (event.relatedTarget){
             return event.relatedTarget;
         } else if (event.toElement){
@@ -59,11 +59,11 @@ var EventUtil = {
     
     },
     
-    getTarget: function(event){
+    getTarget: function(event){ //触发该事件的节点
         return event.target || event.srcElement;
     },
     
-    getWheelDelta: function(event){
+    getWheelDelta: function(event){ //鼠标滚动距离
         if (event.wheelDelta){
             return (client.engine.opera && client.engine.opera < 9.5 ? -event.wheelDelta : event.wheelDelta);
         } else {
@@ -75,7 +75,7 @@ var EventUtil = {
         if (event.preventDefault){
             event.preventDefault();
         } else {
-            event.returnValue = false;
+            event.returnValue = false; //IE
         }
     },
 
