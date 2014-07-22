5c8a629b1be8f3a0ed7c2b87e4d7832b9c93c780
cubika <receiveresume@163.com>
2014-07-22 16:51:27 +0800

    _$extend方法

--- base/global.js
+++ base/global.js
@@ -94,17 +94,57 @@ var f = function() {
 			// 添加静态方法，用于实现继承
 			_class._$extend = function(_super, _static) {
 				if(_isNotFunction(_super)) return;
+				// 从_super父类中继承静态方法
 				if(_static == null || !!_static) {
 					NEJ.X(this, _super, _isNotFunction);
 				}
 				this._$super = _super;
 				this._$supro = _super.prototype;
+				// 将this的原型指向一个新的对象，并将其constructor属性指向this
 				var _parent = function(){};
 				_parent.prototype = _super.prototype;
 				this.prototype = new _parent();
 				var _prototype = this.prototype;
 				_prototype.constructor = this;
 
+				// 为什么__init不直接调用父类的__init方法？
+				var _tmp;
+				for(var x in _mmap) {
+					// 找到x对应的调用父类的相应方法
+					_tmp = _getMethodName(_mmap[x], _umap);
+					if(!_tmp || !this._$supro[x]) continue;
+					// 定义__init等方法，内部要调用__superInit的方法
+					_prototype[x] = (function(_name) {
+						return function() {
+							this[_name].apply(this, arguments);
+						};
+					})(_tmp);
+				}
+				var _pmap = {};
+				for(var x in _umap) {
+					_tmp = _getMethodName(_umap[x], _mmap);
+					if(!_tmp || !this._$supro[_tmp]) continue;
+					_pmap[_tmp] = _super;
+					// 定义__superInit等方法，内部调用父类的__init方法
+					_prototype[x] = (function(_name) {
+						return function() {
+							var _result,
+								_parent = this.__ancestor__[_name],
+								_method = _parent.prototype[_name];
+							this.__ancestor__[_name] = _parent._$super || _super;
+							if(!!_method) {
+								_result = _method.apply(this, arguments);
+							}
+							this.__ancestor__[_name] = _super;
+							return _result;
+						};
+					})(_tmp);
+				}
+
+				_prototype.__cp__ = function() {
+					this.__ancestor__ = NEJ.X({}, _pmap);
+				};
+				_prototype.__super = _prototype.__supInit;
 				return _prototype;
 			};
 			// 最后返回这个函数/类，也就是说 NEJ.C() == _class
